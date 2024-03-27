6ae21c7d90705ff7b63ee1f3ce6c4d466a3e786c
shintaii <info@gecab.nl>
2024-03-27 09:30:34 +0100

    final submission

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ def main():
     with open('books/frankenstein.txt') as f:
         file = f.read()
 
+        book_dict = count_letters(file)
+
+        book_dict_list = [{"letter": k, "num": v} for k,v in book_dict.items()]
+
+        book_dict_list.sort(reverse=True, key=sort_on)
+
         print('--- Begin report of books/frankenstein.txt ---')
-        print(f'{number_of_words(file)} words found in the document')
-        print(count_letters(file))
+        print(f'{number_of_words(file)} words found in the document\n')
+        for i in book_dict_list:
+            if i["letter"].isalpha():
+                print(f'The {i["letter"]} character was found {i["num"]} times')
+
+        print('--- End report ---')
 
 
 def number_of_words(file):
@@ -21,7 +31,11 @@ def count_letters(file):
             letter_hash[letter.lower()] = 1
 
     return letter_hash
-        
+
+def sort_on(dict):
+    return dict["num"]
+
+
 
 
 main()
